1084bd7ca21921eaca5cc55f80cddea9c7da6ed5
Precious Heard <pheard@wlapcs.org>
2018-01-24 20:20:36 +0000

    done

--- sketch.js
+++ sketch.js
@@ -1,8 +1,30 @@
+var precious, brooke, rickya;
+
 function setup(){
     createCanvas(600,400);
     background(0);
+
+    precious = createSlider(0, 255, 125);
+    precious.position(20, 20);
+    
+    brooke = createSlider(0, 255, 133);
+    brooke.position(20, 50);
+    
+    rickya = createSlider(0, 255, 146);
+    rickya.position(20, 80);
 }
 
 function draw(){
+    pv = precious.value();
+    bv = brooke.value();
+    rv = rickya.value();
+
+    background(pv, bv, rv);
+    
+    textSize(30);
+    textAlign(CENTER, CENTER);
+    message = pv + " , " + bv + " , " + rv;
+    text(message, width/2, height/2);
     
 }
+
